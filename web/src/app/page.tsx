40b7297a7ef16e02b917e64561605e2ecb39d9bb
OtengCloud <isaacnewtonoteng@gmail.com>
2026-04-09 17:40:14 +0000

    feat: add image between features and FAQ sections

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -6,6 +6,7 @@ import FeaturesSection from '@/components/FeaturesSection'
 import FaqSection from '@/components/FaqSection'
 import DownloadsSection from '@/components/DownloadsSection'
 import Footer from '@/components/Footer'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -16,6 +17,19 @@ export default function Home() {
         <AboutSection />
         <SolutionsSection />
         <FeaturesSection />
+
+        {/* Feature section image */}
+        <div className="w-full">
+          <Image
+            src="/images/african-business-male-people-shaking-hands.jpg"
+            alt="African business people shaking hands"
+            width={1920}
+            height={800}
+            style={{ width: '100%', height: 'auto', display: 'block' }}
+            sizes="100vw"
+          />
+        </div>
+
         <FaqSection />
         <DownloadsSection />
       </main>
